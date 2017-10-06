0a47735ce08281221c4ebd84beaed4d125a1db47
Soumaila Ramadan <ramadansum@gmail.com>
2017-10-06 17:33:19 +0200

    Ajout de l'aspect Log et Constantes pour environnement (dev,pro ...)

---
+++ src/main/java/fr/istic/m2il/weekendplanning/config/EnvironmentConstants.java
@@ -0,0 +1,16 @@
+package fr.istic.m2il.weekendplanning.config;
+
+public class EnvironmentConstants {
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_TEST = "test";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    public static final String SPRING_PROFILE_CLOUD = "cloud";
+    public static final String SPRING_PROFILE_HEROKU = "heroku";
+    public static final String SPRING_PROFILE_SWAGGER = "swagger";
+    public static final String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
+    public static final String SPRING_PROFILE_K8S = "k8s";
+
+    public EnvironmentConstants() {
+    }
+}
