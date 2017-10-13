8e9b83d0c157cfbef20c8964f737512026034bd0
waberi <waberto>
2017-10-13 21:54:24 +0200

    Page d'authentication

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: LoginFormComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
