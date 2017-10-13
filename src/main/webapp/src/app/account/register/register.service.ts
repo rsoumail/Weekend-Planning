b5c93f82dd56b069e3af40ffb1148ce4da58714a
Soumaila Ramadan <ramadansum@gmail.com>
2017-10-13 15:18:37 +0200

    AJout du compoent register

---
+++ src/main/webapp/src/app/account/register/register.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+import {
+  Http,
+  Headers,
+  RequestOptions,
+  Response
+} from '@angular/http';
+
+@Injectable()
+export class RegisterService {
+
+  constructor() { }
+
+}
