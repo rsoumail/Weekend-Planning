36151a50f60e7a7d42bde18ed342237d23016ecc
Waberi <waberi.houssein@gmail.com>
2017-10-15 21:30:33 +0200

    Ajout du routing

---
+++ src/main/webapp/src/app/app.routing.ts
@@ -0,0 +1,25 @@
+import {RegisterComponent } from './account';
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ActivityComponent} from './activity/activity.component';
+import {PlaceComponent} from './place/place.component';
+import { LoginComponent } from './shared';
+
+
+const appRoutes: Routes = [
+ {
+   path: 'register',
+   component: RegisterComponent
+ },
+ {
+   path: 'login',
+   component: LoginComponent
+ },
+ {
+   path: 'lieu',
+   component: PlaceComponent
+ },
+ {path: 'activity', component: ActivityComponent},
+];
+
+export const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
