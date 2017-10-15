9360817bda41a8a13f621d0b141efd451266514c
Soumaila Ramadan <ramadansum@gmail.com>
2017-10-15 21:42:57 +0200

    Structuration du code et utilisation de constant global

--- src/main/webapp/src/app/place/place.component.ts
+++ src/main/webapp/src/app/place/place.component.ts
@@ -1,7 +1,13 @@
+<<<<<<< f9c685bbb619c8b05000299c9db63cc9850f4636
 import {Component, OnInit} from '@angular/core';
 import {HttpParams, HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+=======
+import { Component, OnInit } from '@angular/core';
+import { HttpParams, HttpClient } from '@angular/common/http';
+>>>>>>> Structuration du code et utilisation de constant global
 import 'rxjs/add/operator/map';
+import { PLACE_API_URL } from './../app.constants';
 @Component({
   selector: 'app-place',
   templateUrl: './place.component.html',
@@ -11,8 +17,8 @@ export class PlaceComponent implements OnInit {
   userChoices = new Set();
   private urlCommunes: string;
   private urlDepartements: string;
-  private urlRegions = 'https://geo.api.gouv.fr/regions?fields=nom,code&format=json';
-  private urlInfo = 'https://geo.api.gouv.fr/communes/?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centrexhttps://geo.api.gouv.fr/communes/?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre&code=';
+  private urlRegions = PLACE_API_URL + 'regions?fields=nom,code&format=json';
+  private urlInfo = PLACE_API_URL + 'communes/?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centrexhttps://geo.api.gouv.fr/communes/?fields=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=json&geometry=centre&code=';
   listRegions;
   listDepartements;
   listCommunes;
@@ -22,7 +28,11 @@ export class PlaceComponent implements OnInit {
   selectedDep = null;
   selectedCom = null;
   params = new HttpParams();
+<<<<<<< f9c685bbb619c8b05000299c9db63cc9850f4636
   constructor(private http: HttpClient, private router: Router) {}
+=======
+  constructor(private http: HttpClient) { }
+>>>>>>> Structuration du code et utilisation de constant global
 
   ngOnInit() {
     this.http.get(this.urlRegions).subscribe(data => this.listRegions = data);
@@ -30,7 +40,7 @@ export class PlaceComponent implements OnInit {
   chargerDep() {
     //    this.params.set('code', this.selectedRegion.code);
     console.log(this.selectedRegion.code);
-    this.urlDepartements = 'https://geo.api.gouv.fr/regions/'.concat(this.selectedRegion.code).concat('/departements?fields=nom,code');
+    this.urlDepartements = PLACE_API_URL + '/regions/'.concat(this.selectedRegion.code).concat('/departements?fields=nom,code');
     this.http.get(this.urlDepartements).subscribe(data => this.listDepartements = data);
     this.information = 'la region: '.concat(this.selectedRegion.nom);
     this.selected = this.selectedRegion;
@@ -38,7 +48,7 @@ export class PlaceComponent implements OnInit {
 
   chargerCom() {
     console.log(this.selectedDep.code);
-    this.urlCommunes = 'https://geo.api.gouv.fr/departements/'.concat(this.selectedDep.code).concat('/communes?fields=nom,code');
+    this.urlCommunes = PLACE_API_URL + 'departements/'.concat(this.selectedDep.code).concat('/communes?fields=nom,code');
     this.http.get(this.urlCommunes).subscribe(data => this.listCommunes = data);
     this.information = 'le departement: '.concat(this.selectedDep.nom);
     this.selected = this.selectedDep;
