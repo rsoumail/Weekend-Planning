c7d9f16b987cb1a8f1dd9514e0d785881347ddf2
Soumaila Ramadan <ramadansum@gmail.com>
2017-10-15 21:47:05 +0200

    Ajout de l'url des lieux comme constante

--- src/main/webapp/src/app/app.constants.ts
+++ src/main/webapp/src/app/app.constants.ts
@@ -1,8 +1,3 @@
-<<<<<<< 9440505cf10fcac9fe89ac0b049364654e94aa6f
-export const SERVER_API_URL = 'localhost:8080/';
-=======
-
 
 export const SERVER_API_URL = "localhost:8080/";
 export const PLACE_API_URL = "https://geo.api.gouv.fr/";
->>>>>>> Mise à jour de la vue et des imports
