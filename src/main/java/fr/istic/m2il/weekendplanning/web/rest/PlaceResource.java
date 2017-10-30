e16bf4363e6f57b1c00f92bd414f2dadcb6d1022
Waberi <waberi.houssein@gmail.com>
2017-10-30 13:25:12 +0100

    en commentaire la classe placeressource

--- src/main/java/fr/istic/m2il/weekendplanning/web/rest/PlaceResource.java
+++ src/main/java/fr/istic/m2il/weekendplanning/web/rest/PlaceResource.java
@@ -1,4 +1,24 @@
 package fr.istic.m2il.weekendplanning.web.rest;
 
 public class PlaceResource {
+	
+//	@Autowired
+//    private final UserService userService;
+//    @Autowired
+//    private final UserRepository userRepository;
+//    
+//	public PlaceResource(UserService userService, UserRepository userRepository) {
+//		this.userService = userService;
+//		this.userRepository = userRepository;
+//	}
+//	
+////	@PostMapping("/places")
+////	public ResponseEntity choisePlaces(@Valid @RequestMapping ManagedUserVM managedUserVM) throws URISyntaxException{
+////		if(managedUserVM.getId() != null) {
+////			return ResponseEntity.badRequest()
+////					.
+////		}
+////	}
+//    
+    
 }
