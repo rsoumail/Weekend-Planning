dc8e3cee2ab0761951d72416a3622a8c2b052e35
Soumaila Ramadan <ramadansum@gmail.com>
2017-11-07 23:05:18 +0100

    Ajout de champs et de amélioration du code

--- src/main/webapp/src/app/place/place.service.ts
+++ src/main/webapp/src/app/place/place.service.ts
@@ -13,10 +13,12 @@ import { SERVER_API_URL } from '../app.constants';
 @Injectable()
 export class PlaceService {
 
+  private resourceUrl = SERVER_API_URL + 'api/places';
   constructor(private http: Http) { }
 
   getUserPlace(user: any): Observable<any> {
     return this.http.get(SERVER_API_URL + 'api/users_places', user);
   }
+
 
 }
