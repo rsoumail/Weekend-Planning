ddfa2dab1e4a29d70aaaba15553b4b296bb54b95
Waberi <waberi.houssein@gmail.com>
2017-11-13 00:14:58 +0100

    dockerize

--- src/main/docker/init_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/usr/bin/mysqld_safe --skip-grant-tables &
-sleep 5
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS weekendplanning"
-mysql -u root weekendplanning < /tmp/weenkendplanning.sql
