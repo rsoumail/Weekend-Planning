d7e0dc6cb255a9ae64ecd74fb10fef1f67880931
Ramadan Soumaila <ramadansum@gmail.com>
2018-04-27 16:31:05 +0200

    add gitignore on remote repo

--- bin/src/main/webapp/src/app/account/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './register/register.component';
-export * from './register/register.service';
-export * from './account.module';
--- bin/src/main/webapp/src/app/app.constants.ts
+++
@@ -1,3 +0,0 @@
-
-export const SERVER_API_URL = '/';
-export const PLACE_API_URL = 'https://geo.api.gouv.fr/';
